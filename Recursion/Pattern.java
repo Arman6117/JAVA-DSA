164ebbd18c3075b0a21e44f19e197416f1e53840
Arman Patel <vanixgaming634@gmail.com>
2025-01-03 18:43:15 +0530

    Pattern and bubble sort

--- Recursion/Pattern.java
+++ Recursion/Pattern.java
@@ -1,18 +1,19 @@
 package Recursion;
-
 public class Pattern {
-    public static void main(String[] args) {
-        triangle1(4, 0);
+    public static void printPattern(int row, int col) {
+        if (row < 1) return;
+        if (col < row) {
+            printPattern(row, col + 1);
+            System.out.print("* ");
+
+        } else {
+            printPattern(row - 1, 0);
+        System.out.println();
+
+        }
     }
 
-    public static void triangle1(int row, int col) {
-        if (row < 1) return;
-        if (col >= row) {
-            System.out.println();
-            triangle1(row - 1, 0);
-        } else {
-            System.out.print(" * ");
-            triangle1(row, col + 1);
-        }
+    public static void main(String[] args) {
+        printPattern(5, 0);
     }
 }
