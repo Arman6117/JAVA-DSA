1f4ca68c46eb4622ba4d3a409e6c4ef251b13b35
Arman Patel <vanixgaming634@gmail.com>
2025-01-04 20:31:46 +0530

    String substring

--- Recursion/RemoveSubString.java
+++ Recursion/RemoveSubString.java
@@ -1,18 +1,20 @@
 package Recursion;
 
 public class RemoveSubString {
-    static  String removeSub(String str, StringBuilder ans, int i) {
-        if (i == str.length()) return ans.toString();
+    static  String removeSub(String str) {
+         if (str.isEmpty()) return "";
 
-        if (!str.startsWith("APPLE")) {
-            ans.append(str.charAt(i));
-        }
-        return  removeSub(str.substring(5),ans,i +1);
-    }
+         if (str.startsWith("APPLE")) {
+             return removeSub(str.substring(5));
+         } else {
+             return str.charAt(0) + removeSub(str.substring(1));
+         }
+
+          }
     public static void main(String[] args) {
         String str = "SUBSTRINGAPPLE";
         String ans = "";
-        ans = removeSub(str, new StringBuilder(), 0);
+        ans = removeSub(str);
         System.out.println(ans);
 
     }
