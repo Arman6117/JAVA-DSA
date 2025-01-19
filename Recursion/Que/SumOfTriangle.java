8dbd3dda5d84bc7caa59a5606f5d88d4e4e6a63c
Arman Patel <vanixgaming634@gmail.com>
2025-01-19 19:05:32 +0530

    Question

--- Recursion/Que/SumOfTriangle.java
+++ Recursion/Que/SumOfTriangle.java
@@ -1,5 +1,26 @@
 package Recursion.Que;
 
+import java.util.Arrays;
+
 public class SumOfTriangle {
-//    int A =
+    public static void main(String[] args) {
+      int [] a = {1,2,3};
+      printTriangle(a);
+    }
+
+    static  void printTriangle (int[]a ) {
+        if(a.length < 1) {
+            return;
+        }
+
+        int[] temp = new int[a.length- 1];
+
+        for (int i = 0; i < a.length -1;i++) {
+            int x = a[i] + a[i + 1];
+            temp[i] = x;
+        }
+
+        printTriangle(temp);
+        System.out.println(Arrays.toString(a));
+    }
 }
