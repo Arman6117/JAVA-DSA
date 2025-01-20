fe4e5941071c75f7d33c5b5b4cafb51fd5d05f93
Arman Patel <vanixgaming634@gmail.com>
2025-01-20 19:42:39 +0530

    Printing maze path

--- Backtracking/MazeCount.java
+++ Backtracking/MazeCount.java
@@ -3,15 +3,28 @@ package Backtracking;
 public class MazeCount {
 
     public static void main(String[] args) {
-        System.out.println(count(3,3));
+        printPath("", 3, 3);
     }
 
-    static int count (int r,int c) {
-        if(r == 1 || c == 1) return 1;
+    static int count(int r, int c) {
+        if (r == 1 || c == 1) return 1;
 
-        int left = count(r-1,c);
-        int right= count(r,c-1);
+        int left = count(r - 1, c);
+        int right = count(r, c - 1);
 
         return left + right;
+    }
+
+    static void printPath(String path, int r, int c) {
+        if (r == 1 && c == 1) {
+            System.out.println(path);
+            return;
+        }
+        if (r > 1) {
+            printPath(path + "D", r-1,c);
+        }
+        if (c > 1) {
+            printPath(path + "R", r , c-1);
+        }
     }
 }
