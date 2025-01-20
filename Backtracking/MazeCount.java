906dbedff04ff5645c2de405f620c8e738bffb13
Arman Patel <vanixgaming634@gmail.com>
2025-01-20 20:09:33 +0530

    Printing maze path down right and diagonally

--- Backtracking/MazeCount.java
+++ Backtracking/MazeCount.java
@@ -3,7 +3,8 @@ package Backtracking;
 public class MazeCount {
 
     public static void main(String[] args) {
-        printPath("", 3, 3);
+        printPathDownAndRight("", 2, 2);
+        printPathDownRightAndDiagonally("", 2,2);
     }
 
     static int count(int r, int c) {
@@ -15,16 +16,33 @@ public class MazeCount {
         return left + right;
     }
 
-    static void printPath(String path, int r, int c) {
+    static void printPathDownAndRight(String path, int r, int c) {
         if (r == 1 && c == 1) {
             System.out.println(path);
             return;
         }
+        //Going down and right
         if (r > 1) {
-            printPath(path + "D", r-1,c);
+            printPathDownAndRight(path + "D", r-1,c);
         }
         if (c > 1) {
-            printPath(path + "R", r , c-1);
+            printPathDownAndRight(path + "R", r , c-1);
+        }
+    }
+
+    static  void printPathDownRightAndDiagonally(String path,int r, int c) {
+        if (r == 1 && c == 1) {
+            System.out.println(path);
+            return;
+        }
+        if (r > 1 && c > 1) {
+            printPathDownRightAndDiagonally(path + "D", r-1,c-1);
+        }
+        if (r > 1) {
+            printPathDownRightAndDiagonally(path + "D", r-1,c);
+        }
+        if (c > 1) {
+            printPathDownRightAndDiagonally(path + "R", r , c-1);
         }
     }
 }
